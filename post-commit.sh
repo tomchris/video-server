146c23d7ad7080d75fd3dddbdd58a2156c336042
Tom Christopoulos <tom.christopoulos@platform9.com>
2018-07-12 12:11:33 -0400

    Updating key

--- post-commit.sh
+++ post-commit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Pushing Repo!"
 git push
-TOKEN="gAAAAABbR3k3-2TWwBx1Nj0T5p4RXPrV8VF16fTt77sr6o8avBVpn63XFVNfKO42zGz0dzSvs9pWzQd-AkEbSr3i3kxWJmlmwzEpDL0T4eEJVISvDFyZzzhOdrGrCEzPH2KHMkxpJ5lBsUwxU2qTpC17wqzLtxU7bg"
+TOKEN="gAAAAABbR32CDY1-CJ6CnnGzpAb8g9nxEYk9eXxcxkenoQ0WLoL-tkzJ-YW0U6wsR3BiQOLPf0tdl85r4eCHFS87L17f3KKI8IenhRmHdhsYmRdHrK1hP6tGovBDyqWLjjfgU89vbtUandACZ-ZAf8PjMIma1beQJmHEgCNi31PPkpQYQx21hKU"
 echo "Creating Instance"
 
 curl -v -k -H "Content-Type:application/json" -H "X-Auth-Token:$TOKEN" -d "@user-data.json" -X POST https://spglobal.platform9.net/nova/v2.1/0bdc6b9977f74d058aa2c57b76271819/servers
